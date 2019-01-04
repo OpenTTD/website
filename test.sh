bb99656642c0c34af1867412578967586aba84f8
Patric Stout <truebrain@openttd.org>
2019-01-04 19:21:30 +0100

    Remove: test.sh is no longer used, and part of Dockerfile / azure-pipelines.yml

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo "Running test build for Docker image ..."
-docker build --pull --no-cache --force-rm -t openttd/website:testrun . \
-    && docker rmi openttd/website:testrun
